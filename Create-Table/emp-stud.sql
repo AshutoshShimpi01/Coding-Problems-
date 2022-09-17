414469b902394f33bbc20068d32c63173c554677
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-09-17 19:29:26 +0530

    Update emp-stud.sql

--- Create-Table/emp-stud.sql
+++ Create-Table/emp-stud.sql
@@ -1,3 +1,5 @@
+
+  
 -- Online SQL Editor to Run SQL Online.
 -- Use the editor to create new tables, insert data and all other SQL operations.
   
@@ -16,7 +18,7 @@ create table student
   reg_year integer,
   grade text,
   hod text,
-  foreign key(emp_id) references employee(emp_id)
+  foreign key(student_id) references employee(emp_id)
   
 );
 
