425d4d5f96a24532927dd6c39e08f49a4b8d361d
Ashutosh Shimpi <94462032+AshutoshShimpi01@users.noreply.github.com>
2022-09-19 15:20:41 +0530

    Create student.sql

---
+++ Create-Table/student.sql
@@ -0,0 +1,36 @@
+Create table student
+(
+
+Student_id integer,
+Name text,
+Last text,
+Coe text,
+Primary key(Student_id)
+
+);
+
+
+
+
+
+
+
+
+alter table student add gpa decimal(2,3);
+
+insert into student values(1,'ashu','shimpi','sits',8.43);
+insert into student values(2,'vinod','khade','sits',10);
+
+insert into student values(3,'dhiraj','shinde','sits',9.2);
+
+select * from student;
+
+
+
+
+
+-- Output
+-- 1|ashu|shimpi|sits|8.43
+-- 2|vinod|khade|sits|10
+-- 3|dhiraj|shinde|sits|9.2
+
